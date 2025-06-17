ae00e1340cdf57dbf4ad91ce0054e9a387045ef2
Adyatma Yusuf Farabi <151417193+1Envy2@users.noreply.github.com>
2025-06-17 22:33:14 +0700

    file database update

--- finote.sql
+++ finote.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 17, 2025 at 12:13 PM
+-- Generation Time: Jun 17, 2025 at 03:31 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -28,27 +28,25 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `categories` (
-  `id` int UNSIGNED NOT NULL,
-  `user_id` int UNSIGNED DEFAULT NULL,
-  `name` varchar(100) NOT NULL,
-  `type` enum('income','expense') NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `type` enum('income','expense') NOT NULL DEFAULT 'expense'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id`, `user_id`, `name`, `type`, `created_at`) VALUES
-(1, NULL, 'Gaji', 'income', '2025-06-10 16:19:36'),
-(2, NULL, 'Investasi', 'income', '2025-06-10 16:19:36'),
-(3, NULL, 'Bonus', 'income', '2025-06-10 16:19:36'),
-(4, NULL, 'Pendapatan Orang Tua', 'income', '2025-06-10 16:19:36'),
-(5, NULL, 'Makanan', 'expense', '2025-06-10 16:19:36'),
-(6, NULL, 'Transportasi', 'expense', '2025-06-10 16:19:36'),
-(7, NULL, 'Tagihan Listrik', 'expense', '2025-06-10 16:19:36'),
-(8, NULL, 'Jajan', 'expense', '2025-06-10 16:19:36'),
-(9, NULL, 'Hiburan', 'expense', '2025-06-10 16:19:36');
+INSERT INTO `categories` (`id`, `name`, `created_at`, `type`) VALUES
+(1, 'Gaji', '2025-06-17 20:26:25', 'income'),
+(2, 'Makanan & Minuman', '2025-06-17 20:26:25', 'expense'),
+(3, 'Transportasi', '2025-06-17 20:26:25', 'expense'),
+(5, 'Hiburan', '2025-06-17 20:26:25', 'expense'),
+(6, 'Tagihan Listrik', '2025-06-17 20:26:25', 'expense'),
+(7, 'Cicilan', '2025-06-17 20:26:25', 'expense'),
+(8, 'Investasi', '2025-06-17 20:26:25', 'income'),
+(9, 'Belanja Online', '2025-06-17 21:13:40', 'expense');
 
 -- --------------------------------------------------------
 
@@ -57,14 +55,14 @@ INSERT INTO `categories` (`id`, `user_id`, `name`, `type`, `created_at`) VALUES
 --
 
 CREATE TABLE `transactions` (
-  `id` int UNSIGNED NOT NULL,
-  `user_id` int UNSIGNED NOT NULL,
-  `category_id` int UNSIGNED NOT NULL,
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `category_id` int DEFAULT NULL,
   `type` enum('income','expense') NOT NULL,
-  `amount` decimal(15,2) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
+  `amount` decimal(15,2) NOT NULL,
+  `description` text,
   `transaction_date` date NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -72,16 +70,8 @@ CREATE TABLE `transactions` (
 --
 
 INSERT INTO `transactions` (`id`, `user_id`, `category_id`, `type`, `amount`, `description`, `transaction_date`, `created_at`) VALUES
-(1, 1, 1, 'income', 1500000.00, 'gaji bulan juni', '2025-06-10', '2025-06-10 16:24:59'),
-(2, 1, 8, 'expense', 15000.00, 'beli seblak', '2025-06-10', '2025-06-10 16:25:35'),
-(3, 1, 9, 'expense', 20000.00, 'nonton', '2025-06-11', '2025-06-11 00:59:02'),
-(4, 1, 6, 'expense', 1000000.00, 'servis motor', '2025-06-11', '2025-06-11 01:00:23'),
-(5, 1, 4, 'income', 1500000.00, 'gaji bulan juni orang tua', '2025-06-11', '2025-06-11 01:01:54'),
-(6, 4, 4, 'income', 1500000.00, 'gaji orang tua', '2025-06-11', '2025-06-11 01:51:41'),
-(7, 4, 5, 'expense', 20000.00, 'beli soto', '2025-06-11', '2025-06-11 01:52:19'),
-(8, 1, 3, 'income', 200000.00, 'Tip', '2025-06-17', '2025-06-17 07:23:29'),
-(9, 2, 4, 'income', 10000000000.00, 'pendapatan ortu', '2025-06-17', '2025-06-17 08:10:51'),
-(10, 2, 4, 'income', 10000000000.00, 'pendapatan ortu', '2025-06-17', '2025-06-17 08:11:04');
+(5, 3, 1, 'income', '1500000.00', 'gaji bulan juni', '2025-06-17', '2025-06-17 22:07:08'),
+(6, 3, 7, 'expense', '1000000.00', '', '2025-06-17', '2025-06-17 22:07:23');
 
 -- --------------------------------------------------------
 
@@ -90,24 +80,22 @@ INSERT INTO `transactions` (`id`, `user_id`, `category_id`, `type`, `amount`, `d
 --
 
 CREATE TABLE `users` (
-  `id` int UNSIGNED NOT NULL,
+  `id` int NOT NULL,
   `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `photo` varchar(255) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL
+  `role` enum('user','admin') NOT NULL DEFAULT 'user',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `created_at`, `photo`, `phone`) VALUES
-(1, 'ady', '$2y$10$2wj1l5iX3cfTZk6HbVjFlufHPUHszJuZT6pRtcxjeQZH4sVUkIN7W', 'ady@gmail.com', '2025-06-10 02:24:13', 'Logo Unila.jpg', '0888888888'),
-(2, 'salsha nabila', '$2y$10$kMZueoHO1VVUOdCgjGPc6.XmYbAOcc5PL8NOp.HTlZzBn1cXHR.D2', 'salsha@gmail.com', '2025-06-10 02:25:58', 'profile.jpeg', '0812345678911'),
-(3, 'abyan', '$2y$10$2J9QEiPnIOTmqJNIx5FMRuqbczUiA48LDmw/cAFCoWp8BYTJb3l9q', 'admin@example.com', '2025-06-10 14:15:58', NULL, NULL),
-(4, 'joni', '$2y$10$xUO6CfTXKBzaS6VI.v7vlObNoAeWWWxp/88OKV0qepfP5QdjY7z02', 'joni@gmail.com', '2025-06-11 01:50:55', NULL, NULL);
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `photo`, `role`, `created_at`) VALUES
+(1, 'admin', 'admin@finote.com', '$2y$10$z30K1tYThh6wk2ZWH72fsOZ5adu7svkNgHlyQU5gQ1bjk8I4NTDSC', NULL, 'admin', '2025-06-17 20:26:25'),
+(3, 'ady', 'ady@example.com', '$2y$10$zKwpuksGEb5zPCt8mHmb8.vt.uD3n.uUACbcPobnSroQWk/NR9MV6', NULL, 'user', '2025-06-17 21:04:40');
 
 --
 -- Indexes for dumped tables
@@ -118,7 +106,7 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `created_at`, `photo
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `transactions`
@@ -133,7 +121,8 @@ ALTER TABLE `transactions`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -143,36 +132,30 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `categories`
---
-ALTER TABLE `categories`
-  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
 -- Constraints for table `transactions`
 --
 ALTER TABLE `transactions`
   ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE RESTRICT;
+  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
